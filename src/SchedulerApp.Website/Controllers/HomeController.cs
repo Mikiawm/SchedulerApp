22a1224143815d8c63c9346d907b8aba6ebe0a57
Michał Sobański <michal.sobanski@atos.net>
2017-03-10 18:48:16 +0100

    Add autofac, not working.

--- src/SchedulerApp.Website/Controllers/HomeController.cs
+++ src/SchedulerApp.Website/Controllers/HomeController.cs
@@ -11,10 +11,25 @@ namespace SchedulerApp.Website.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IContactService contactServices;
+        private readonly IContactService contactService;
+
+        public HomeController(IContactService contactService)
+        {
+            this.contactService = contactService;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Contact> contacts;
+            contacts = contactService.GetContacts();
+            ViewData["Message"] = "Your contact page.";
+            if (contacts != null)
+            {
+                return View(ContactViewModel.DisplayContacts(contacts));
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public IActionResult About()
@@ -22,15 +37,21 @@ namespace SchedulerApp.Website.Controllers
             ViewData["Message"] = "Your application description page.";
 
             return View();
-        }
+        }   
 
         public IActionResult Contact()
         {
-
-            var contacts = contactServices.GetContacts();
+            IEnumerable<Contact> contacts;
+            contacts = contactService.GetContacts();
             ViewData["Message"] = "Your contact page.";
-            
-            return View(ContactViewModel.DisplayContacts(contacts));
+            if(contacts != null)
+            {
+                return View(ContactViewModel.DisplayContacts(contacts));
+            }
+            else
+            {
+                return View();
+            }
         }
 
 
--- src/SchedulerApp.Website/Startup.cs
+++ src/SchedulerApp.Website/Startup.cs
@@ -9,6 +9,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SchedulerApp.Data;
 using Microsoft.EntityFrameworkCore;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using System.Reflection;
+using SchedulerApp.Data.Infrastructure;
+using SchedulerApp.Data.Repositories;
+using SchedulerApp.Domain.Services;
 
 namespace SchedulerApp.Website
 {
@@ -33,19 +39,39 @@ namespace SchedulerApp.Website
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
             services.AddMvc();
             services.AddApplicationInsightsTelemetry(Configuration);
 
             var connection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = SchedulerApp; Integrated Security=True;Connect Timeout=15;";
-            
+
             services.AddDbContext<SchedulerContext>(options => options.UseSqlServer(connection, b => b.MigrationsAssembly("SchedulerApp.Website")));
+            return SetAutofacContainer(services);
+
+        }
+        public IContainer ApplicationContainer { get; private set; }
+        private IServiceProvider SetAutofacContainer(IServiceCollection services)
+        {
+            var builder = new ContainerBuilder();
+
+            // Register dependencies, populate the services from
+            // the collection, and build the container. If you want
+            // to dispose of the container at the end of the app,
+            // be sure to keep a reference to it as a property or field.
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+            builder.Populate(services);
+            builder.RegisterType<ContactServices>().As<IContactService>();
+            this.ApplicationContainer = builder.Build();
+
+            // Create the IServiceProvider based on the container.
+            return new AutofacServiceProvider(this.ApplicationContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime appLifetime)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -72,6 +98,7 @@ namespace SchedulerApp.Website
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+            appLifetime.ApplicationStopped.Register(() => this.ApplicationContainer.Dispose());
         }
     }
 }

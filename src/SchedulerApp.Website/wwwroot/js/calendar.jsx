a58d1c5b56047fc2f9c6ff7ee5ce333de154b004
Michał Sobański <Mikiawm@gmail.com>
2017-05-09 23:40:05 +0200

    changes

--- src/SchedulerApp.Website/wwwroot/js/calendar.jsx
+++ src/SchedulerApp.Website/wwwroot/js/calendar.jsx
@@ -1,13 +1,15 @@
 ﻿var Calendar = React.createClass({
-    calc: function (year, month) {},
-    componentWillMount: function () {},
-    componentDidMount: function () {},
+    calc: function (year, month) { },
+    componentWillMount: function () { },
+    componentDidMount: function () { },
     componentDidUpdate: function (prevProps, prevState) { },
     getInitialState: function () {
         var date = new Date();
+        var daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
         return {
             year: date.getFullYear(),
             month: date.getMonth(),
+            daysInMonth: daysInMonths[date.getMonth()],
             selectedYear: date.getFullYear(),
             selectedMonth: date.getMonth(),
             selectedDate: date.getDate(),
@@ -19,19 +21,18 @@
             dayNames: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
             monthNames: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
             monthNamesFull: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-            firstOfMonth: null,
-            daysInMonth: null
+            firstOfMonth: null
         };
     },
-    getPrev: function () {},
-    getNext: function () {},
-    selectDate: function (year, month, date, element) {},
+    getPrev: function () { },
+    getNext: function () { },
+    selectDate: function (year, month, date, element) { },
     render: function () {
         return (
             <div className="r-calendar">
                 <div className="r-inner">
                     <Header monthNames={this.state.monthNamesFull} month={this.state.month} year={this.state.year} onPrev={this.getPrev} onNext={this.getNext} />
-                    <WeekDays dayNames={this.state.dayNames} startDay={this.state.startDay} weekNumbers={this.state.weekNumbers} />
+                    <WeekDays dayNames={this.state.dayNames} startDay={this.state.startDay} weekNumbers={this.state.weekNumbers} daysInMonth={this.state.daysInMonth} />
                     <MonthDates month={this.state.month} year={this.state.year} daysInMonth={this.state.daysInMonth} firstOfMonth={this.state.firstOfMonth} startDay={this.state.startDay} onSelect={this.selectDate} weekNumbers={this.state.weekNumbers} disablePast={this.state.disablePast} minDate={this.state.minDate} />
                 </div>
             </div>
@@ -40,12 +41,24 @@
 });
 
 var Header = React.createClass({
-    render: function () {return (<div>{this.props.monthNames[this.props.month]}</div>)}
+    render: function () { return (<div>{this.props.monthNames[this.props.month]}</div>) }
 });
 
 var WeekDays = React.createClass({
-    render: function () {return null}
+    render: function () {
+        
+        return (<div>{this.props.daysInMonth}</div>)
+
+    }
 });
+
+//var Day = React.createClass({
+//    render: function () {
+//        return (
+//                <div style="width: 50px; height: 50px"></div>
+//    )
+//    }
+//});
 
 var MonthDates = React.createClass({
     statics: {
@@ -54,7 +67,9 @@ var MonthDates = React.createClass({
         date: new Date().getDate(),
         today: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())
     },
-    render: function () {return (
-        <div></div>
-        )}
+    render: function () {
+        return (
+                <div></div>
+        )
+    }
 });
